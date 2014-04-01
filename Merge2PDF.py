8d04609cb29a0aae4e81abbd99974bd45b6183b5
musary <churui.ctec@gmail.com>
2014-04-01 22:13:23 +0800

    V0.1
    Finally I decided to call this script Version 0.1!
    It could work! No matter where you put this directory!
    WOW!

--- Merge2PDF.py
+++ Merge2PDF.py
@@ -1,27 +1,42 @@
 # coding: utf-8
-import csv
-from PyPDF2 import PdfFileWriter, PdfFileReader
+import csv, os, time
+from pyPdf import PdfFileWriter, PdfFileReader
 
+#getting current Merge2PDF.py derectory.
+#make directories for source file and merged file
+pyDt = os.getcwd()
+sourceDt = pyDt + '/source'
+mergedDt = pyDt + '/merged'
+
+#open pages.csv and read page numbers.
 lst = []
-csvfile = file('pages.csv', 'rb')
+csvfile = file(sourceDt + '/pages.csv', 'rb')
 reader = csv.reader(csvfile)
 for line in reader:
 	a = int(line[0])	
 	lst.append(a)
-
 csvfile.close() 
 
-input1 = PdfFileReader(file("before.pdf", "rb"))
-input2 = PdfFileReader(file("after.pdf", "rb"))
-output = PdfFileWriter()
 
+#manipulate before.pdf and after.pdf
+#note: for runing in company computer, renmae before and after file with
+#extension .xls for some reason we all know.
+input1 = PdfFileReader(file(sourceDt+"/before.pdf", "rb"))
+input2 = PdfFileReader(file(sourceDt+"/after.pdf", "rb"))
+output = PdfFileWriter()
 
 for i in lst:
 	output.addPage(input1.getPage(i-1))
-
 for i in lst:
         output.addPage(input2.getPage(i-1))
 
-outputStream1 = file(r"Merged.pdf", "wb")
-output.write(outputStream1)
-outputStream1.close()
+#get time in format year-mon-day_hour-minute-sec
+localtime = time.strftime('%Y-%m-%d_%X',time.localtime(time.time()))
+localtime = localtime.replace(':','-')
+
+outputname = mergedDt + "/Merged_" + localtime + ".pdf"
+outputStream = file(outputname,"wb")
+output.write(outputStream)
+outputStream.close()
+
+print "It's done!"
